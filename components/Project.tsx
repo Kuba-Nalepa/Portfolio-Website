18c6c5f95c1f05528772ed044a60b18e40bb07f8
Kuba-Nalepa <nalepak11@gmail.com>
2022-12-06 19:03:22 +0100

    add responsiveness

--- components/Project.tsx
+++ components/Project.tsx
@@ -6,13 +6,13 @@ export default function Project(props:{
     images: string[];
 }) {
     return(
-    <div className="w-3/4 flex flex-row items-center mt-24 px-8 bg-slate-300 rounded-xl shadow-md shadow-black border-t-2 border-white">
-        <div className="w-1/3 my-8">
+    <div className=" w-10/12 flex flex-col items-center mt-24 px-2 py-8 bg-slate-300 rounded-xl shadow-md shadow-black border-t-2 border-white md:w-3/4 md:flex-row">
+        <div className=" flex justify-center my-8 md:w-1/3">
             <Gallery images={props.images} />
         </div>
 
-        <div className="w-2/3 px-8">
-            <p className="mt-8 text-center text-black text-xl font-bold">{props.title}</p>
+        <div className="md:w-2/3 md:px-8">
+            <p className="mt-2 pb-4 text-center text-black text-4xl font-bold ">{props.title}</p>
             <p className="text-center text-black text-lg">{props.desc} </p>
         </div>
     </div>  
